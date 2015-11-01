ce1772ddd69c1d67d33cdd13503a8a0b55a702c0
jfpp83 <jfpp83@gmail.com>
2015-11-01 19:29:41 -0300

    stock

---
+++ Stock/src/com/stock/Pedido.java
@@ -0,0 +1,27 @@
+package com.stock;
+
+public class Pedido {
+	
+	public long IdProducto;
+	
+	public int Cantidad;
+
+	public long getIdProducto() {
+		return IdProducto;
+	}
+
+	public void setIdProducto(long idProducto) {
+		IdProducto = idProducto;
+	}
+
+	public int getCantidad() {
+		return Cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		Cantidad = cantidad;
+	}
+	
+
+
+}
---
+++ Stock/src/com/stock/ResultadoAnulacion.java
@@ -0,0 +1,9 @@
+package com.stock;
+
+public class ResultadoAnulacion {
+	
+	public String Codigo;
+	
+	public String Descripcion;
+
+}
