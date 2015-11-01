22444932661dbea114e1a39dae8af209bc2322fc
gastonnic <gastonnic@gmail.com>
2015-11-01 17:52:46 -0300

    .

--- epuerto.bat
+++ epuerto.bat
@@ -3,14 +3,13 @@ cd "C:\Users\gastonnic\workspace\ePuerto\src"
 if not exist "C:\Middleware\testdir\recepcion" mkdir C:\Middleware\testdir\recepcion
 if not exist "C:\Middleware\testdir\confirmar" mkdir C:\Middleware\testdir\confirmar
 if not exist "C:\Middleware\testdir\anular" mkdir C:\Middleware\testdir\anular
-dir
 if not exist "ePuerto\WatchDir.class" javac ePuerto\WatchDir.java
 
 @ECHO off 
 title MiddleWare folder watcher 
 color 9 
 cls
-echo Escuchando la carpeta recepcion... 
+echo Escuchando la carpeta recepcion...  
 echo.
  
 start cmd /k  "cd "C:\Users\gastonnic\workspace\ePuerto\src" & title MiddleWare folder watcher & color 9 & echo Escuchando la carpeta confirmar...& echo. & java ePuerto.WatchDir "C:\Middleware\testdir\confirmar" "
