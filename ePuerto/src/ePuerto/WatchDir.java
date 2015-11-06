0d51bb24e79541247a362513277651581af5c4fc
gastonnic <gastonnic@gmail.com>
2015-11-05 23:32:18 -0300

    cambios

--- ePuerto/src/ePuerto/WatchDir.java
+++ ePuerto/src/ePuerto/WatchDir.java
@@ -118,14 +118,36 @@ public class WatchDir {
                 
                 if (kind == ENTRY_CREATE){
                 	if (dir.getFileName().toString().equals("recepcion")){
-                		Thread.sleep(5000);
-                		Files.copy(child, Paths.get(dir.getParent().toString()+"/confirmar/"+child.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+                		//Thread.sleep(5000);
+                		System.out.println("\n La orden "+child.getFileName().toString()+" fue agregada...\n");
+                		//Files.copy(child, Paths.get(dir.getParent().toString()+"/confirmar/"+child.getFileName()), StandardCopyOption.REPLACE_EXISTING);
                 	}
                 	else if (dir.getFileName().toString().equals("confirmar")) {
-						
+                		File f = new File(Paths.get(dir.getParent().toString()+"/recepcion/"+child.getFileName()).toString());
+						if (! f.exists()){							
+							System.out.println("\n     !!!     La orden que quiere confirmar no existe     !!!     \n");}
+						else{
+							System.out.println("\n La orden "+child.getFileName().toString()+" esta siendo confirmada...\n");
+							Thread.sleep(5000);
+// cambiar el copy delete x un move
+							Files.copy(child, Paths.get(dir.getParent().toString()+"/procesados/"+child.getFileName()), StandardCopyOption.REPLACE_EXISTING);							
+							System.out.println("\n La orden "+child.getFileName().toString()+" fue procesada ...\n");							
+							Files.delete(Paths.get(dir.getParent().toString()+"/recepcion/"+child.getFileName()));
+							Files.delete(child);
+						}
 					}
                 	else if (dir.getFileName().toString().equals("anular")) {
-						
+                		File f = new File(Paths.get(dir.getParent().toString()+"/recepcion/"+child.getFileName()).toString());
+						if (! f.exists()){							
+							System.out.println("\n     !!!     la orden que quiere anular no existe     !!!     \n");}
+						else{
+							System.out.println("\n La orden "+child.getFileName().toString()+" esta siendo anulada...\n");
+							Thread.sleep(5000);
+							Files.copy(child, Paths.get(dir.getParent().toString()+"/procesados/"+child.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+							System.out.println("\n La orden "+child.getFileName().toString()+" fue procesada ...\n");							
+							Files.delete(Paths.get(dir.getParent().toString()+"/recepcion/"+child.getFileName()));
+							Files.delete(child);
+						}
 					}
                 }
                 
